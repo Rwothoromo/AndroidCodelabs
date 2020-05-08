d8b45db62da7a3b2bb0862ccfe9fe0a3d6873b76
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2020-05-08 19:08:50 +0300

    Display a button to Show Final Output

--- android-workmanager/app/src/main/java/com/example/background/BlurActivity.kt
+++ android-workmanager/app/src/main/java/com/example/background/BlurActivity.kt
@@ -16,6 +16,7 @@
 
 package com.example.background
 
+import android.content.Intent
 import android.os.Bundle
 import android.view.View
 import androidx.appcompat.app.AppCompatActivity
@@ -49,6 +50,16 @@ class BlurActivity : AppCompatActivity() {
 
         // Show work status
         viewModel.outputWorkInfos.observe(this, workInfosObserver())
+
+        // To view the blurred imaged
+        binding.seeFileButton.setOnClickListener {
+            viewModel.outputUri?.let { currentUri ->
+                val actionView = Intent(Intent.ACTION_VIEW, currentUri)
+                actionView.resolveActivity(packageManager)?.run {
+                    startActivity(actionView)
+                }
+            }
+        }
     }
 
     /**
@@ -92,6 +103,16 @@ class BlurActivity : AppCompatActivity() {
 
             if (workInfo.state.isFinished) {
                 showWorkFinished()
+
+                // Normally this processing, which is not directly related to drawing views on
+                // screen would be in the ViewModel. For simplicity we are keeping it here.
+                val outputImageUri = workInfo.outputData.getString(KEY_IMAGE_URI)
+
+                // If there is an output file show "See File" button
+                if (!outputImageUri.isNullOrEmpty()) {
+                    viewModel.setOutputUri(outputImageUri as String)
+                    binding.seeFileButton.visibility = View.VISIBLE
+                }
             } else {
                 showWorkInProgress()
             }
