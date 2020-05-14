8c8abaaf3642e25713a3dde6c08b7764e8e2e9bc
Elijah Rwothoromo <rwothoromoelijah@gmail.com>
2020-05-14 22:26:38 +0300

    Use InstantTaskExecutorRule to assert LiveData

--- android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
+++ android-testing/app/src/test/java/com/example/android/architecture/blueprints/todoapp/tasks/TasksViewModelTest.kt
@@ -1,7 +1,14 @@
 package com.example.android.architecture.blueprints.todoapp.tasks
 
+import androidx.arch.core.executor.testing.InstantTaskExecutorRule
+import androidx.lifecycle.Observer
 import androidx.test.core.app.ApplicationProvider
 import androidx.test.ext.junit.runners.AndroidJUnit4
+import com.example.android.architecture.blueprints.todoapp.Event
+import org.hamcrest.MatcherAssert.assertThat
+import org.hamcrest.Matchers.not
+import org.hamcrest.Matchers.nullValue
+import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
 
@@ -14,6 +21,16 @@ import org.junit.runner.RunWith
  */
 @RunWith(AndroidJUnit4::class)
 class TasksViewModelTest {
+    // InstantTaskExecutorRule is a JUnit Rule.
+    // When you use it with the @get:Rule annotation,
+    // it causes some code in the InstantTaskExecutorRule class to be run before and after the tests.
+    //
+    // This rule runs all Architecture Components-related background jobs in the same thread
+    // so that the test results happen synchronously, and in a repeatable order.
+    //
+    // When you write tests that include testing LiveData, use this rule!
+    @get:Rule
+    var instantExecutorRule = InstantTaskExecutorRule()
 
     @Test
     fun addNewTask_setsNewTaskEvent() {
@@ -21,11 +38,33 @@ class TasksViewModelTest {
         // Given a fresh TasksViewModel
         val tasksViewModel = TasksViewModel(ApplicationProvider.getApplicationContext())
 
+//        // Create observer - no need for it to do anything!
+//        val observer = Observer<Event<Unit>> {}
+//        try {
+//
+//            // Observe the LiveData forever
+//            tasksViewModel.newTaskEvent.observeForever(observer)
+//
+//            // When adding a new task
+//            tasksViewModel.addNewTask()
+//
+//            // Then the new task event is triggered
+//            val value = tasksViewModel.newTaskEvent.value
+//            assertThat(value?.getContentIfNotHandled(), (not(nullValue())))
+//
+//        } finally {
+//            // Whatever happens, don't forget to remove the observer!
+//            tasksViewModel.newTaskEvent.removeObserver(observer)
+//        }
+
+        // Instead of using the above commented out chunck of code, use a LiveDataTestUtil as shown below
         // When adding a new task
         tasksViewModel.addNewTask()
 
         // Then the new task event is triggered
-        // TODO test LiveData
+        val value = tasksViewModel.newTaskEvent.getOrAwaitValue()
+
+        assertThat(value.getContentIfNotHandled(), not(nullValue()))
     }
 
 }
